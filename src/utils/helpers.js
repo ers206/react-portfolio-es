e2bb88aebecbbaa3b52c79d6ea145feaf27a1823
Eric Schweitzer <eric_schweitzer@yahoo.com>
2022-10-09 16:23:00 -0500

    just started 3 errors

---
+++ src/utils/helpers.js
@@ -0,0 +1,9 @@
+// not sure if any of this is nessacary from mod setup 20.1.3
+// export function capitalizeFirstLetter(string) {
+//     return string.charAt(0).toUpperCase() + string.slice(1);
+//   }
+  
+//   export function validateEmail(email) {
+//     var re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+//     return re.test(String(email).toLowerCase());
+//   }
