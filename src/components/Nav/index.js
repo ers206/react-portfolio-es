398159f6fd9d5fa220fca7382c24ab794eac8779
Eric Schweitzer <eric_schweitzer@yahoo.com>
2022-10-19 20:20:26 -0500

    tests not working

--- src/components/Nav/index.js
+++ src/components/Nav/index.js
@@ -20,7 +20,7 @@ function Nav(props) {
     <header className="flex-row px-1">
       <h2>
         <a data-testid="link" href="/">
-          <span role="img" aria-label="camera"> 📸</span> Oh Snap!
+          <span role="img" aria-label="camera"> 💻</span> Eric Schweitzer Portfolio
         </a>
       </h2>
       <nav>
